2cf8a788fd6fd8e2b1199abffacd9c5963c2a967
ayeshanoor123656 <217932949+ayeshanoor123656@users.noreply.github.com>
2026-06-24 03:11:41 +0500

    production api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://inkwell-backend-kotj.onrender.com/"
 });
 
 export default API;
